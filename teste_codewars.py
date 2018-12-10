2d687b7dfc756607ffc9463a04f7012039c1d0fb
Andrei Bamboi <Andrei.Bamboi@endava.com>
2018-12-10 09:58:57 +0200

    Update

--- teste_codewars.py
+++ teste_codewars.py
@@ -1,8 +1,20 @@
-infile = open(r"./Client_script/file.txt", "r")
-line = infile.readline()
-while line:
-    values = line.split()
-    print('QB ', values[0], values[1], 'had a rating of ', values[2] )
-    line = infile.readline()
 
-infile.close()
+'''
+spam, ham = 'yum', 'YUM'
+[sspam, hham] = ['yyum', 'YYUM']
+a,b, c, d = 'spam'
+T, *t = 'spam'
+print(spam, sspam, ham, hham)
+print(a)
+print(t)
+'''
+import sys
+olista= [1,2,3]
+temp = sys.stdout
+sys.stdout = open('fisier.txt', 'w')
+print(olista)
+sys.stdout = temp
+print(open('fisier.txt').read())
+print('%s %s %s' % ('ta', 'ra', 'rap'))
+print('{0} {1} {2}'.format('bla', 'cla' , 'ta'))
+
